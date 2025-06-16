0d46ba4087b0c8f763113726bff04c8419c5df99
Peter Rudolf <peterrudolf249@gmail.com>
2025-06-16 14:07:23 -0400

    Ensure _redirects is copied to dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import copy from 'rollup-plugin-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    copy({
+      targets: [
+        { src: 'static/_redirects', dest: 'dist' }
+      ],
+      hook: 'writeBundle'
+    })
+  ],
+  // other Vite settings
 })
